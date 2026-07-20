5fd0a9b9e866d11b632121552458bbb246cb590d
Stepan <srekhlyasov@gmail.com>
2026-07-20 12:05:35 +0400

    scroll-fix

--- frontend/src/shared/ui/templates/PageTemplate.tsx
+++ frontend/src/shared/ui/templates/PageTemplate.tsx
@@ -12,7 +12,7 @@ export function PageTemplate({
   return (
     <Box>
       <AppHeader sx={{ position: 'fixed', top: 0, left: 0, right: 0, zIndex: 100 }} />
-      <Container maxWidth={maxWidth} sx={{ mt: 10 }}>
+      <Container maxWidth={maxWidth}  sx={{ overflow: 'auto', height: 'calc(100vh - 64px)', mt: '64px' }}>
         <Box sx={{ py: 4 }}>
         {title && (<Typography variant="h4" component="h1" gutterBottom>
             {title}
