335d54c5a2cc43fa715680b593073a7baac606dc
Stepan <srekhlyasov@gmail.com>
2026-07-20 12:11:13 +0400

    upd

--- frontend/src/shared/ui/templates/PageTemplate.tsx
+++ frontend/src/shared/ui/templates/PageTemplate.tsx
@@ -9,10 +9,11 @@ export function PageTemplate({
   children,
   maxWidth = 'lg',
 }: PropsWithChildren<{ title?: string; maxWidth?: Breakpoint }>) {
+  const headerHeight = '64px';
   return (
-    <Box>
-      <AppHeader sx={{ position: 'fixed', top: 0, left: 0, right: 0, zIndex: 100 }} />
-      <Container maxWidth={maxWidth}  sx={{ overflow: 'auto', height: 'calc(100vh - 64px)', mt: '64px' }}>
+    <Box sx={{ overflow: 'auto', mt: headerHeight }}>
+      <AppHeader sx={{ position: 'fixed', top: 0, left: 0, right: 0, zIndex: 100, height: headerHeight }} />
+      <Container maxWidth={maxWidth}  sx={{ height: `calc(100vh - ${headerHeight})` }}>
         <Box sx={{ py: 4 }}>
         {title && (<Typography variant="h4" component="h1" gutterBottom>
             {title}
